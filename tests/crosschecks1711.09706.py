bf6d27d599d14cd600e7c0b303e63b46b1b032b7
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2021-11-19 15:00:35 +0100

    added more intermediate steps in the deltaPhi calculations and corrected no accretion case calculations

--- tests/crosschecks1711.09706.py
+++ tests/crosschecks1711.09706.py
@@ -76,9 +76,9 @@ def plotDiffEq(sp, r0, r1):
     print(c_gw*ms.year_to_pc, c_df*ms.year_to_pc)
     l, = plt.loglog(r/sp.r_isco(), np.abs(inspiral.Classic.dE_gw_dt(sp, r))/inspiral.Classic.dE_orbit_da(sp, r), label=r'$dE_{gw}/dt / dE_{orbit}/dR$', alpha=0.5)
     plt.loglog(r/sp.r_isco(), c_gw*f_gw(x, alpha)/eps**(1./(3.-alpha)) , label='$c_{gw}f_{gw}$', color=l.get_c(), linestyle='--')
-    l, = plt.loglog(r/sp.r_isco(), np.abs(inspiral.Classic.dE_df_dt(sp, r))/inspiral.Classic.dE_orbit_da(sp, r), label=r'$dE_{df}/dt / dE_{orbit}/dR$', alpha=0.5)
+    l, = plt.loglog(r/sp.r_isco(), np.abs(inspiral.Classic.dE_force_dt(sp, inspiral.Classic.F_df, r))/inspiral.Classic.dE_orbit_da(sp, r), label=r'$dE_{df}/dt / dE_{orbit}/dR$', alpha=0.5)
     plt.loglog(r/sp.r_isco(), c_df* f_df(x, alpha)/eps**(1./(3.-alpha)), label='$c_{df}f_{df}$' , color=l.get_c(), linestyle='--')
-    l, = plt.loglog(r/sp.r_isco(), np.abs(inspiral.Classic.dE_acc_dt(sp, r))/inspiral.Classic.dE_orbit_da(sp, r), label=r'$dE_{acc}/dt / dE_{orbit}/dR$', alpha=0.5)
+    l, = plt.loglog(r/sp.r_isco(), np.abs(inspiral.Classic.dE_force_dt(sp, inspiral.Classic.F_acc, r))/inspiral.Classic.dE_orbit_da(sp, r), label=r'$dE_{acc}/dt / dE_{orbit}/dR$', alpha=0.5)
     plt.loglog(r/sp.r_isco(), c_df* f_df(x, alpha)*b_A(sp, x, alpha)/eps**(1./(3.-alpha)), label='$c_{df}f_{df}b_A$' , color=l.get_c(), linestyle='--')
     plt.xlabel('$r/r_{ISCO}$')
 
@@ -121,9 +121,6 @@ def L(sp, f, accretion=True):
         If accretion=False, then L' as given by eq (58)
     """
     alpha = sp.halo.alpha
-    #eps = F(sp)/Meff(sp)
-    #c_gw, c_df, ctild = coeffs(sp)
-    #c_eps = Meff(sp, 2.*sp.r_isco())**(11./6.-1./3.*alpha) * ctild * eps**((11.-2.*alpha)/(6.-2.*alpha))
     c_eps = 5.*np.pi/32. * Meff(sp)**(-(alpha+5.)/3.) * sp.halo.rho_spike * sp.halo.r_spike**(alpha) * inspiral.Classic.ln_Lambda
     if accretion:
         # TODO: Check prefactor, it's in (36) but not (51)
@@ -159,7 +156,7 @@ def getPhaseParameters(sp, ev, f_c = None):
     Calculates the terms involved in the derivation of Delta Phi, as the Phi (second part of eq (28b)), 2pi tf (first part of eq(28b), tilde{Phi} (eq(28b)), Delta tilde{Phi} as in eq (56)
     for the solution of the numerical evolution
     """
-    omega_s = sp.omega_s(ev.R)
+    omega_s = sp.omega_s(ev.R) # in this call it is important whether sp.m2 is an array or a single number
     f_gw = omega_s/np.pi
     if f_c is None:
         t = ev.t
@@ -168,18 +165,11 @@ def getPhaseParameters(sp, ev, f_c = None):
         omega_s = omega_s[:len(f_gw)]
         t  = ev.t[:len(f_gw)]
 
-    if isinstance(sp.m_chirp(), (np.ndarray, Sequence)):
-        m_chirp = sp.m_chirp()[0]
-    else:
-        m_chirp = sp.m_chirp()
-
     omega_gw = interp1d(t, 2*omega_s, kind='cubic', bounds_error=False, fill_value=(0.,0.) )
-    Phit = np.cumsum([quad(lambda t: omega_gw(t), t[i-1], t[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i == 0 else 0. for i in range(len(t)) ])
-    #Phit = solve_ivp(lambda t,y: omega_gw(t), [t[0], t[-1]], [0.], t_eval=t, atol=1e-13, rtol=1e-13).y[0]
-    Phi = Phit #- Phit[-1]
+    Phi = np.cumsum([quad(lambda t: omega_gw(t), t[i-1], t[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i == 0 else 0. for i in range(len(t)) ])
+    Phi = Phi - Phi[-1]   # normalize to Phi being zero at f_c
 
-    # Use t_c = t[-1] as reference point
-    tpt = 2.*np.pi*f_gw * (t - t[-1])
+    tpt = 2.*np.pi*f_gw * (t - t[-1])  # use t_c = t[-1] as reference point
 
     PhiTild = tpt - Phi
     return f_gw, Phi, tpt, PhiTild
@@ -196,57 +186,63 @@ def plotPhase(sp, ev_acc, ev_nacc, f_c=None):
 
     f_c = f_gw[-1]
     PhiTild0 =  (8.*np.pi*sp.m_chirp())**(-5./3.) * (-3./4. * f_gw**(-5./3.) - 5./4. * f_gw * f_c**(-8./3.) + 2.*f_c**(-5./3.))
-    #PhiTild0 = - 3./4.*(8.*np.pi*sp.m_chirp()*f_gw)**(-5./3.) + 3./4.*(8.*np.pi*sp.m_chirp()*f_c)**(-5./3.)
     DeltaPhi = PhiTild - PhiTild0
 
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, Phi, label=r'$\Phi^{code}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, tpt, label=r'$2\pi t^{code}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, PhiTild, label=r'$\tilde{\Phi}^{code}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, np.abs(DeltaPhi), label=r'$\Delta\tilde{\Phi}^{code}$')
+    # plt.plot(f_gw/ms.hz_to_invpc, Phi, label=r'$\Phi^{code}$')
+    # plt.plot(f_gw/ms.hz_to_invpc, tpt, label=r'$2\pi t^{code}$')
+    plt.plot(f_gw/ms.hz_to_invpc, PhiTild, label=r'$\tilde{\Phi}^{code}$')
+    plt.plot(f_gw/ms.hz_to_invpc, np.abs(DeltaPhi), label=r'$\Delta\tilde{\Phi}^{code}$')
 
+    # Paper accretion
     mu_interp = interp1d(f_gw, mu(sp, f_gw, f_gw[0]), kind='cubic', bounds_error=False, fill_value='extrapolate')
-    #Phi_ana = np.cumsum([quad(lambda f: f**(-8./3.)/L(sp, f)/mu_interp(f), f_gw[i-1], f_gw[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i == 0 else 0. for i in range(len(f_gw)) ][::-1])[::-1]
-    Phi_ana = solve_ivp(lambda f,y: -f**(-8./3.)/L(sp, f)/mu_interp(f), [f_gw[-1], f_gw[0]], [0.], t_eval=f_gw[::-1], atol=1e-13, rtol=1e-13).y[0][::-1]
-    print("Phi_ana=", Phi_ana)
-    Phi_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * (Phi_ana )
-
-    #tpt_ana = np.cumsum([quad(lambda f: f**(-11./3.)/L(sp, f)/mu_interp(f), f_gw[i-1], f_gw[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i==0 else 0. for i in range(len(f_gw)) ][::-1])[::-1]
-    tpt_ana = solve_ivp(lambda f,y: -f**(-11./3.)/L(sp, f)/mu_interp(f), [f_gw[-1], f_gw[0]], [0.], t_eval=f_gw[::-1], atol=1e-13, rtol=1e-13).y[0][::-1]
-    print("tpt_ana=", tpt_ana)
-    tpt_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * f_gw * ( tpt_ana)
+    Phi_ana = solve_ivp(lambda f,y: f**(-8./3.)/L(sp, f)/mu_interp(f), [f_gw[-1], f_gw[0]], [0.], t_eval=f_gw[::-1], atol=1e-13, rtol=1e-13).y[0][::-1]
+    Phi_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * Phi_ana
+    tpt_ana = solve_ivp(lambda f,y: f**(-11./3.)/L(sp, f)/mu_interp(f), [f_gw[-1], f_gw[0]], [0.], t_eval=f_gw[::-1], atol=1e-13, rtol=1e-13).y[0][::-1]
+    tpt_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * f_gw *  tpt_ana
     PhiTild_ana = tpt_ana - Phi_ana
     DeltaPhi_ana = PhiTild_ana - PhiTild0
 
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, Phi_ana, label=r'$\Phi^{paper}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, tpt_ana, label=r'$2\pi t^{paper}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, PhiTild_ana, label=r'$\tilde{\Phi}^{paper}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, np.abs(DeltaPhi_ana), label=r'$\Delta\tilde{\Phi}^{paper}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, np.abs(DeltaPhi - DeltaPhi_ana), label=r'$\Delta \Delta\tilde{\Phi}$')
+    # plt.plot(f_gw/ms.hz_to_invpc, Phi_ana, label=r'$\Phi^{paper}$')
+    # plt.plot(f_gw/ms.hz_to_invpc, tpt_ana, label=r'$2\pi t^{paper}$')
+    plt.plot(f_gw/ms.hz_to_invpc, PhiTild_ana, label=r'$\tilde{\Phi}^{paper}$')
+    plt.plot(f_gw/ms.hz_to_invpc, np.abs(DeltaPhi_ana), label=r'$\Delta\tilde{\Phi}^{paper}$')
+    plt.plot(f_gw/ms.hz_to_invpc, np.abs(DeltaPhi - DeltaPhi_ana), label=r'$\Delta \Delta\tilde{\Phi}$')
 
     # Code no accretion
-    f_gw_nacc, _, __, PhiTild_nacc = getPhaseParameters(sp, ev_nacc, f_c=f_c)
+    f_gw_nacc, Phi_nacc, tpt_nacc, PhiTild_nacc = getPhaseParameters(sp, ev_nacc, f_c=f_c)
+
     f_c = f_gw_nacc[-1]
     PhiTild0 =  (8.*np.pi*sp.m_chirp())**(-5./3.) * (-3./4. * f_gw_nacc**(-5./3.) - 5./4. * f_gw_nacc * f_c**(-8./3.) + 2.*f_c**(-5./3.))
-    #PhiTild0 = - 3./4.*(8.*np.pi*sp.m_chirp()*f_gw_nacc)**(-5./3.) + 3./4.*(8.*np.pi*sp.m_chirp()*f_c)**(-5./3.)
     DeltaPhi_nacc = PhiTild_nacc - PhiTild0
-    DeltaPhi_naccinterp = interp1d(f_gw_nacc, DeltaPhi_nacc, kind='cubic', bounds_error=False, fill_value=(0.,0.))
-    deltaPhi = np.abs(DeltaPhi_naccinterp(f_gw) - DeltaPhi)
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, deltaPhi, label=r'$\delta\tilde{\Phi}^{code}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, np.abs(deltaPhi/DeltaPhi), label=r'$\delta\tilde{\Phi}^{code}/\Delta\tilde{\Phi}$')
+    #DeltaPhi_naccinterp = interp1d(f_gw_nacc, DeltaPhi_nacc, kind='cubic', bounds_error=False, fill_value=(0.,0.))
+    #deltaPhi = np.abs(DeltaPhi_naccinterp(f_gw) - DeltaPhi)
+    PhiTild_nacc_interp = interp1d(f_gw_nacc, PhiTild_nacc, kind='cubic', bounds_error=False, fill_value=(0.,0.))
+    deltaPhi = np.abs(PhiTild_nacc_interp(f_gw) - PhiTild)
+
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, Phi_nacc, label=r'$\Phi_{nacc}^{code}$')
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, tpt_nacc, label=r'$2\pi t_{nacc}^{code}$')
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, PhiTild_nacc, label=r'$\tilde{\Phi}_{nacc}^{code}$')
+    plt.plot(f_gw/ms.hz_to_invpc, deltaPhi, label=r'$\delta\tilde{\Phi}^{code}$')
+    plt.plot(f_gw/ms.hz_to_invpc, np.abs(deltaPhi/DeltaPhi), label=r'$\delta\tilde{\Phi}^{code}/\Delta\tilde{\Phi}$')
 
     # Paper no accretion
-    #Phi_ana = np.cumsum([quad(lambda f: f**(-8./3.)/L(sp, f, accretion=False), f_gw_nacc[i-1], f_gw_nacc[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i == 0 else 0. for i in range(len(f_gw_nacc)) ][::-1])[::-1]
-    Phi_ana = solve_ivp(lambda f,y: -f**(-8./3.)/L(sp, f, accretion=False), [f_gw_nacc[-1], f_gw_nacc[0]], [0.], t_eval=f_gw_nacc[::-1], atol=1e-13, rtol=1e-13, method='LSODA').y[0][::-1]
-    Phi_ana = 10./3. * (8.*np.pi*sp.m_chirp())**(-5./3.) * (Phi_ana - Phi_ana[-1])
-    #tpt_ana = np.cumsum([quad(lambda f: f**(-11./3.)/L(sp, f, accretion=False), f_gw_nacc[i-1], f_gw_nacc[i], limit=200, epsrel=1e-13, epsabs=1e-13)[0] if not i==0 else 0. for i in range(len(f_gw_nacc)) ][::-1])[::-1]
-    tpt_ana = solve_ivp(lambda f,y: -f**(-11./3.)/L(sp, f, accretion=False), [f_gw_nacc[-1], f_gw_nacc[0]], [0.], t_eval=f_gw_nacc[::-1], atol=1e-13, rtol=1e-13, method='LSODA').y[0][::-1]
-    tpt_ana = 10./3. * (8.*np.pi*sp.m_chirp())**(-5./3.) * f_gw_nacc * ( tpt_ana - tpt_ana[-1])
-    PhiTild_nacc_ana = tpt_ana - Phi_ana
+    Phi_nacc_ana = solve_ivp(lambda f,y: f**(-8./3.)/L(sp, f, accretion=False)/sp.m2, [f_gw_nacc[-1], f_gw_nacc[0]], [0.], t_eval=f_gw_nacc[::-1], atol=1e-13, rtol=1e-13).y[0][::-1] # added mass so that the values are above atol
+    Phi_nacc_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * Phi_nacc_ana
+    tpt_nacc_ana = solve_ivp(lambda f,y: f**(-11./3.)/L(sp, f, accretion=False)/sp.m2, [f_gw_nacc[-1], f_gw_nacc[0]], [0.], t_eval=f_gw_nacc[::-1], atol=1e-13, rtol=1e-13).y[0][::-1]
+    tpt_nacc_ana = 10./3. * (8.*np.pi*Meff(sp)**(2./5.))**(-5./3.) * f_gw_nacc *  tpt_nacc_ana
+    PhiTild_nacc_ana = tpt_nacc_ana - Phi_nacc_ana
+
     PhiTild_nacc_anaInterp = interp1d(f_gw_nacc, PhiTild_nacc_ana, kind='cubic', bounds_error=False, fill_value=(0.,0.))
     deltaPhi_ana = np.abs(PhiTild_nacc_anaInterp(f_gw) - PhiTild_ana)
+    #DeltaPhi_nacc_ana = PhiTild_nacc_ana - PhiTild0
+    #DeltaPhi_nacc_anaInterp = interp1d(f_gw_nacc, DeltaPhi_nacc_ana, kind='cubic', bounds_error=False, fill_value=(0.,0.))
+    #deltaPhi_ana = np.abs(DeltaPhi_nacc_anaInterp(f_gw) - DeltaPhi_ana)
 
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, deltaPhi_ana, label=r'$\delta\tilde{\Phi}^{paper}$')
-    plt.plot(f_gw*ms.year_to_pc*3.17e-8, np.abs(deltaPhi_ana/DeltaPhi_ana), label=r'$\delta\tilde{\Phi}^{paper}/\Delta\tilde{\Phi}$')
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, Phi_nacc_ana, label=r'$\Phi_{nacc}^{paper}$')
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, tpt_nacc_ana, label=r'$2\pi t_{nacc}^{paper}$')
+    #plt.plot(f_gw_nacc/ms.hz_to_invpc, PhiTild_nacc_ana, label=r'$\tilde{\Phi}_{nacc}^{paper}$')
+    plt.plot(f_gw/ms.hz_to_invpc, deltaPhi_ana, label=r'$\delta\tilde{\Phi}^{paper}$')
+    plt.plot(f_gw/ms.hz_to_invpc, np.abs(deltaPhi_ana/DeltaPhi_ana), label=r'$\delta\tilde{\Phi}^{paper}/\Delta\tilde{\Phi}$')
 
     plt.xlabel('f')
     #plt.xscale('log')
@@ -259,18 +255,21 @@ def plotWaveform(sp, ev):
     Plots the gravitational waveform of h as given by eq (40) and compares them to the code
     """
     f_gw, h, _, Psi  = waveform.h_2( sp, ev)
-    plt.loglog(f_gw*ms.year_to_pc*3.17e-8, h, label=r'$\tilde{h}^{code}$')
+    plt.loglog(f_gw/ms.hz_to_invpc, h, label=r'$\tilde{h}^{code}$')
 
     alpha = sp.halo.alpha
     eps = F(sp,2.*sp.r_isco())/Meff(sp)
     A = (5./24.)**(1./2.) * np.pi**(-2./3.) /sp.D * sp.m_chirp()**(5./6.)
-    plt.loglog(f_gw*ms.year_to_pc*3.17e-8, A*f_gw**(-7./6.) * (L(sp,f_gw))**(-1./2.), label=r'$\tilde{h}^{paper,approx}$')
+    plt.loglog(f_gw/ms.hz_to_invpc, A*f_gw**(-7./6.) * (L(sp,f_gw))**(-1./2.), label=r'$\tilde{h}^{paper,approx}$')
 
     delta = (Meff(sp)/np.pi**2 / f_gw**2)**(1.-alpha/3.)
     chi = 1. + delta*eps/3. + (2.-alpha)/9. *delta**2 * eps**2
     x = (delta*eps)**(1./(3.-alpha)) *chi
     c_gw, c_df, ctild = coeffs(sp)
-    plt.loglog(f_gw*ms.year_to_pc*3.17e-8, A*f_gw**(-7./6.) * chi**(19./4.) * (K(x, alpha)* (1. + ctild*J(x, alpha)*(1.+b_A(sp, x, alpha)) ))**(-1./2.), label=r'$\tilde{h}^{paper}$' )
+    h_pap = A*f_gw**(-7./6.) * chi**(19./4.) * (K(x, alpha)* (1. + ctild*J(x, alpha)*(1.+b_A(sp, x, alpha)) ))**(-1./2.)
+    plt.loglog(f_gw/ms.hz_to_invpc, h_pap, label=r'$\tilde{h}^{paper}$' )
+
+    plt.loglog(f_gw/ms.hz_to_invpc, np.abs(h - h_pap), label=r'$\Delta \tilde{h}$')
     plt.ylabel('h'); plt.xlabel('f')
 
 
@@ -278,7 +277,6 @@ m1 = 1e5 *ms.solar_mass_to_pc
 m2 = 10. *ms.solar_mass_to_pc
 D = 1e3
 sp_1 = ms.SystemProp(m1, m2, halo.Spike( 226*ms.solar_mass_to_pc, 0.54, 7./3.), D, includeHaloInTotalMass=True)
-
 
 plt.figure()
 plotDiffEq(sp_1, sp_1.r_isco(), 1e7*sp_1.r_isco())
@@ -288,9 +286,10 @@ plt.figure()
 plotPhiprimeprime(sp_1, sp_1.r_isco(), 1e5*sp_1.r_isco())
 plt.legend(); plt.grid()
 
+
 R0 = 100.*sp_1.r_isco()
-ev_nacc = inspiral.Classic.Evolve(sp_1, R0, a_fin=sp_1.r_isco(), opt=inspiral.Classic.EvolutionOptions(accuracy=1e-13, accretion=False))
-ev_acc = inspiral.Classic.Evolve(sp_1, R0, a_fin=sp_1.r_isco(), opt=inspiral.Classic.EvolutionOptions(accuracy=1e-13, accretion=True, verbose=1))
+ev_nacc = inspiral.Classic.Evolve(sp_1, R0, a_fin=sp_1.r_isco(), opt=inspiral.Classic.EvolutionOptions(accuracy=1e-13, accretion=False, verbose=1))
+ev_acc = inspiral.Classic.Evolve(sp_1, R0, a_fin=sp_1.r_isco(), opt=inspiral.Classic.EvolutionOptions(accuracy=1e-13, accretion=True, accretionRecoilLoss=False, verbose=1))
 
 plt.figure()
 plotPhase(sp_1, ev_acc, ev_nacc, f_c = 0.1*ms.hz_to_invpc)
@@ -309,14 +308,13 @@ plt.loglog(ev_acc.t, np.abs(mu_ana - ev_acc.m2)/m2, label="$\Delta m_2$", linest
 plt.legend(); plt.grid()
 print("mass increase:", ev_acc.m2[-1]/ev_acc.m2[0] -1.)
 
-'''
+
 # Now check the eccentric implementation with a tiny eccentricity, it should be very similar
 a0 = 100.*sp_1.r_isco()
 e0 = 1e-5
-sp_1.m2 = 1.*ms.solar_mass_to_pc
+sp_1.m2 = m2
 
-#t2, a2, e2, m22 = inspiral.Classic.evolve_elliptic_binary(sp_1, a0, e0, sp_1.r_isco(), acc=1e-12, accretion=True)
-ev2 = inspiral.Classic.evolve_elliptic_binary(sp_1, a0, e0, sp_1.r_isco(), acc=1e-12, accretion=True)
+ev2 = inspiral.Classic.Evolve(sp_1, a0, e0, sp_1.r_isco(), opt=inspiral.Classic.EvolutionOptions(accuracy=1e-12, accretion=True, accretionRecoilLoss=False))
 
 plt.figure()
 plt.loglog(ev_acc.t, ev_acc.R, label='R, circular')
@@ -327,7 +325,6 @@ plt.loglog(ev2.t, ev2.m2, label='$m_2$, elliptic')
 
 plt.loglog(ev2.t, ev2.e, label='e')
 plt.grid(); plt.legend()
-'''
 
 plt.show()
 
