5841a77c2b46f1a481a4364a3e06e1890cdf53cb
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2022-06-20 15:32:31 +0200

    changed default integrator for Michel accretion and added velocity and soundspeed

--- src/imripy/halo.py
+++ src/imripy/halo.py
@@ -667,11 +667,13 @@ class MichelAccretion(MatterHalo):
         self.kappa = kappa
         self.Gamma = Gamma
 
-        # See equations in (11.94), (11.96), (11.101) in Rezzolla
+        # See equations in (11.94), (11.96), (11.97), (11.101) in Rezzolla (2013)
         self.u_c = np.sqrt(M/2./r_c)
         c_s2 = self.u_c**2 / (1. - 3*self.u_c**2)
         self.rho_c = (1./Gamma/kappa * (Gamma-1.)*c_s2 / (Gamma-1.-c_s2))**(1./(Gamma-1.))
         self.M_dot = 4.*np.pi * self.r_c**2 * self.rho_c * self.u_c
+        self.rho_infty = (((1. + Gamma*kappa/(Gamma-1.)*self.rho_c**(Gamma-1.))*np.sqrt(1.-2.*self.M/self.r_c + self.u_c**2)
+              -1.) * (Gamma-1.)/Gamma/kappa)**(1./(Gamma-1.))
 
 
     def FromM_dot(M, M_dot, rho_infty, kappa, Gamma):
@@ -719,60 +721,12 @@ class MichelAccretion(MatterHalo):
         return MichelAccretion(M, r_c, kappa, Gamma)
 
 
-    def solve_ode(self, r):
-        """
-        Solves the differential equations for rho, u of the Michel system of equations
-            at the requested radii, see
-            https://arxiv.org/pdf/2102.12529.pdf
-            for reference
-
-        Parameters:
-            r : float or array_like
-                The radii at which rho, u are to be calculated, has to be strictly monotically increasing
-
-        Returns:
-            out : np.ndarray
-                This is a multidimensional array where rho = out[0,:], u = out[1,:]  arrays correspond to r
-        """
-        rho_scale = self.rho_c
-        u_scale = self.u_c
-        def dy_dr(r, y):
-            rho, u = y
-            rho *= rho_scale; u *= u_scale;
-            h = 1. + self.Gamma/(self.Gamma-1)*self.kappa * rho**(self.Gamma-1.)
-
-            x = 1. - 2*self.M/r + u**2
-            alpha = self.Gamma * self.kappa * rho**(self.Gamma-1.) * x / h / u**2
-            du_dr = (2. * alpha * u**2 *r - self.M) / (u*r**2 * (1.-alpha))
-            drho_dr = - rho/r/u * (r*du_dr + 2*u)
-            #print(drho_dr, du_dr)
-            return np.array([drho_dr/rho_scale, du_dr/u_scale])
-
-
-        y_s = np.array([self.rho_c/rho_scale, self.u_c/u_scale])
-
-        if r[0] >= self.r_c:
-            sol = solve_ivp(dy_dr, [self.r_c, r[-1]], y_s, t_eval=r, atol=1e-13, rtol=1e-4)
-            #print("1", sol.message)
-            rho = sol.y[0,:]*rho_scale
-            u = sol.y[1,:]*u_scale
-            return rho, u
-        elif r[-1] <= self.r_c:
-            sol = solve_ivp(dy_dr , [self.r_c, r[0]], y_s, t_eval=r[::-1], atol=1e-13, rtol=1e-4)
-            #print("2", sol.message)
-            rho = (sol.y[0,:]*rho_scale)[::-1]
-            u = (sol.y[1,:]*u_scale)[::-1]
-            return rho,u
-        else:
-            rs= np.split(r, [np.where(r> self.r_c)[0][0]])
-            rho1, u1 = self.solve_ode(rs[0])
-            rho2, u2 = self.solve_ode(rs[1])
-            return np.concatenate([rho1, rho2]), np.concatenate([u1,u2])
-
     # def solve_ode(self, r):
     #     """
     #     Solves the differential equations for rho, u of the Michel system of equations
-    #         at the requested radii, see eq. (11.86) of Rezzolla (2013)
+    #         at the requested radii, see
+    #         https://arxiv.org/pdf/2102.12529.pdf
+    #         for reference
 
     #     Parameters:
     #         r : float or array_like
@@ -782,32 +736,34 @@ class MichelAccretion(MatterHalo):
     #         out : np.ndarray
     #             This is a multidimensional array where rho = out[0,:], u = out[1,:]  arrays correspond to r
     #     """
-    #     def du_dr(r, u):
-    #         W2 = (1. - 2*self.M/r + u**2)
-    #         rho = self.M_dot/(4.*np.pi*r**2 * u)
-    #         cs2 = 1./ ( 1./(self.Gamma*self.kappa* rho**(self.Gamma-1.)) + 1./(self.Gamma-1.) )
-    #         du_dr = - u/r * (2.*W2*cs2 - self.M/r) / (W2*cs2 - u**2)
-    #         #print(f"r={r}, u={u}, W2={W2}, rho={rho}, cs2={cs2}, du_dr={du_dr}")
-    #         return du_dr
+    #     rho_scale = self.rho_c
+    #     u_scale = self.u_c
+    #     def dy_dr(r, y):
+    #         rho, u = y
+    #         rho *= rho_scale; u *= u_scale;
+    #         h = 1. + self.Gamma/(self.Gamma-1)*self.kappa * rho**(self.Gamma-1.)
+
+    #         x = 1. - 2*self.M/r + u**2
+    #         alpha = self.Gamma * self.kappa * rho**(self.Gamma-1.) * x / h / u**2
+    #         du_dr = (2. * alpha * u**2 *r - self.M) / (u*r**2 * (1.-alpha))
+    #         drho_dr = - rho/r/u * (r*du_dr + 2*u)
+    #         #print(drho_dr, du_dr)
+    #         return np.array([drho_dr/rho_scale, du_dr/u_scale])
 
 
-    #     y_c = [self.u_c]
+    #     y_s = np.array([self.rho_c/rho_scale, self.u_c/u_scale])
 
-    #     if r[0] > self.r_c:
-    #         sol = solve_ivp(du_dr, [self.r_c, r[-1]], y_c, t_eval=r, atol=1e-13, rtol=1e-5)
-    #         print("1", sol.message)
-    #         u = sol.y[0]
-    #         rho = self.M_dot / (4.*np.pi*r**2 * u)
+    #     if r[0] >= self.r_c:
+    #         sol = solve_ivp(dy_dr, [self.r_c, r[-1]], y_s, t_eval=r, atol=1e-13, rtol=1e-4)
+    #         #print("1", sol.message)
+    #         rho = sol.y[0,:]*rho_scale
+    #         u = sol.y[1,:]*u_scale
     #         return rho, u
-    #     elif r[-1] < self.r_c:
-    #         #sol = solve_ivp(lambda r,x: du_dr(r, np.exp(x))*np.exp(x) , [self.r_c, r[0]], np.log(y_c), t_eval=r[::-1], rtol=1e-6)
-    #         sol = solve_ivp(lambda r,u: du_dr(r, u) , [self.r_c, r[0]], y_c, t_eval=r[::-1], atol=1e-13, rtol=1e-5)
-    #         print("2", sol.message)
-    #         u = sol.y[0][::-1]
-    #         # sol = odeint(du_dr , y_c, np.append([self.r_c], r[::-1]), tfirst=True)
-    #         # u = sol[1:,0][::-1]
-    #         # print(u)
-    #         rho = self.M_dot / (4.*np.pi*r**2 * u)
+    #     elif r[-1] <= self.r_c:
+    #         sol = solve_ivp(dy_dr , [self.r_c, r[0]], y_s, t_eval=r[::-1], atol=1e-13, rtol=1e-4)
+    #         #print("2", sol.message)
+    #         rho = (sol.y[0,:]*rho_scale)[::-1]
+    #         u = (sol.y[1,:]*u_scale)[::-1]
     #         return rho,u
     #     else:
     #         rs= np.split(r, [np.where(r> self.r_c)[0][0]])
@@ -815,27 +771,77 @@ class MichelAccretion(MatterHalo):
     #         rho2, u2 = self.solve_ode(rs[1])
     #         return np.concatenate([rho1, rho2]), np.concatenate([u1,u2])
 
-    def machNumber(self, r):
+    def solve_ode(self, r):
         """
         Solves the differential equations for rho, u of the Michel system of equations
-            and calculates the Mach number of the ideal gas
+            at the requested radii, see eq. (11.86) of Rezzolla (2013)
 
         Parameters:
             r : float or array_like
-                The radii at which the mach number is to be calculated, has to be strictly monotically increasing
+                The radii at which rho, u are to be calculated, has to be strictly monotically increasing
 
         Returns:
             out : np.ndarray
-                The mach number at the point of interest
+                This is a multidimensional array where rho = out[0,:], u = out[1,:]  arrays correspond to r
         """
-        y = self.solve_ode(r)
-        rho = y[0,:]
-        u = y[1,:]
-        V = u / np.sqrt(u**2 + 1.-2.*self.M/r )
-        h = self.h_infty / np.sqrt(1. - 2.*self.M/r + u**2)
-        C = np.sqrt(self.Gamma*self.kappa * rho**(self.Gamma-1) /h)
-        return V*np.sqrt(1.-C**2) / C / np.sqrt(1. - V**2)
+        def du_dr(r, u):
+            W2 = (1. - 2*self.M/r + u**2)
+            rho = self.M_dot/(4.*np.pi*r**2 * u)
+            cs2 = 1./ ( 1./(self.Gamma*self.kappa* rho**(self.Gamma-1.)) + 1./(self.Gamma-1.) )
+            du_dr = - u/r * (2.*W2*cs2 - self.M/r) / (W2*cs2 - u**2)
+            #print(f"r={r}, u={u}, W2={W2}, rho={rho}, cs2={cs2}, du_dr={du_dr}")
+            return du_dr
 
+
+        y_c = [self.u_c]
+
+        if r[0] > self.r_c:
+            sol = solve_ivp(du_dr, [self.r_c, r[-1]], y_c, t_eval=r, atol=1e-13, rtol=1e-5)
+            #print("1", sol.message)
+            u = sol.y[0]
+            rho = self.M_dot / (4.*np.pi*r**2 * u)
+            return rho, u
+        elif r[-1] < self.r_c:
+            #sol = solve_ivp(lambda r,x: du_dr(r, np.exp(x))*np.exp(x) , [self.r_c, r[0]], np.log(y_c), t_eval=r[::-1], rtol=1e-6)
+            sol = solve_ivp(lambda r,u: du_dr(r, u) , [self.r_c, r[0]], y_c, t_eval=r[::-1], atol=1e-13, rtol=1e-5)
+            #print("2", sol.message)
+            u = sol.y[0][::-1]
+            # sol = odeint(du_dr , y_c, np.append([self.r_c], r[::-1]), tfirst=True)
+            # u = sol[1:,0][::-1]
+            # print(u)
+            rho = self.M_dot / (4.*np.pi*r**2 * u)
+            return rho,u
+        else:
+            rs= np.split(r, [np.where(r> self.r_c)[0][0]])
+            rho1, u1 = self.solve_ode(rs[0])
+            rho2, u2 = self.solve_ode(rs[1])
+            return np.concatenate([rho1, rho2]), np.concatenate([u1,u2])
+
+    def velocity(self, r):
+        """
+        The velocities v_r, v_phi of the particles in the halo
+
+
+        The density function of the Michel accretion halo
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the density
+
+        Returns:
+            v_r : array_like
+                The radial velocity at the radius r
+            v_phi : array_like
+                The angular velocity at the radius r
+        """
+        if not  isinstance(r, (collections.Sequence, np.ndarray)):
+            r = np.array([r])
+        u = self.solve_ode(r)[1]
+        W = np.sqrt(1. - 2.*self.M/r + u**2)
+        v_r = -u/W  # see eq (11.98) in Rezzolla(2013) but with a negative sign since the particles are falling inward
+        v_r = v_r[0] if len(v_r) == 1 else v_r
+        v_phi = np.zeros(np.shape(v_r))
+        return v_r, v_phi
 
     def density(self, r):
         """
@@ -853,6 +859,19 @@ class MichelAccretion(MatterHalo):
             r = np.array([r])
         return self.solve_ode(r)[0]
 
+    def soundspeed(self, r):
+        """
+        The soundspeed c_s of the Michel accretion halo
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the soundspeed
+
+        Returns:
+            out : float or array_like (depending on r)
+                The soundspeed at the radius r
+        """
+        return (1./ self.Gamma/self.kappa/self.density(r)**(self.Gamma-1.) + 1./(self.Gamma-1.))**(-1./2.)
 
     def __str__(self):
         """
