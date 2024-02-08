bdb44c30361349e7663939e693a1b4a611c7fe06
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2024-02-08 16:39:29 +0100

    added angle transformations through frames

--- src/imripy/waveform.py
+++ src/imripy/waveform.py
@@ -3,6 +3,7 @@ from scipy.interpolate import UnivariateSpline, interp1d
 from scipy.integrate import quad, solve_ivp, cumulative_trapezoid
 from scipy.special import jv
 import collections
+from imripy.kepler import KeplerOrbit
 
 
 def h_2(hs, ev, dbg=False, acc=1e-13):
@@ -97,7 +98,8 @@ def h_n(n, hs, ev, dbg=False, acc=1e-13):
     TODO:
         Check redshift, luminosity distance inclusion
     """
-    s_i = np.sin(ev.inclination_angle); c_i = np.cos(ev.inclination_angle)
+    inclination_angle, beta = get_observer_orbit_angles(hs, ev, ev.t)
+    s_i = np.sin(inclination_angle); c_i = np.cos(inclination_angle)
 
     def C_n_plus(n, periapse_angle, e):
         return  (  2.* s_i**2 * jv(n, n*e) +  2./e**2 * (1. + c_i**2) * np.cos(2.*periapse_angle)
@@ -141,8 +143,8 @@ def h_n(n, hs, ev, dbg=False, acc=1e-13):
 
     # the actual waveform
     e = np.clip(ev.e, 1e-10, None) # in case eccentricity vanishes
-    h_n_plus  = A_n * ( C_n_plus(n, ev.periapse_angle, e)   +  1.j  * S_n_plus(n, ev.periapse_angle, e))
-    h_n_cross = A_n * ( C_n_cross(n, ev.periapse_angle, e)  +  1.j  * S_n_cross(n, ev.periapse_angle, e))
+    h_n_plus  = A_n * ( C_n_plus(n, beta, e)   +  1.j  * S_n_plus(n, beta, e))
+    h_n_cross = A_n * ( C_n_cross(n, beta, e)  +  1.j  * S_n_cross(n, beta, e))
 
     # the corresponding observed frequencies
     f_gw = n*F / (1.+hs.z)
@@ -153,9 +155,72 @@ def h_n(n, hs, ev, dbg=False, acc=1e-13):
     return f_gw, h_n_plus, h_n_cross, Psi_n
 
 
+def get_observer_orbit_angles(hs, ev, t_grid):
+    """
+    This funtion calculates the angles between the orbital and observer plane,
+        by going through the fundamental frame
+    The resulting angles beta', iota' describe the angle between the polarization axis in the observer plane and the periapse in the orbital plane,
+        and the inclination of the orbital plane wrt the observer plane.
+    For the math see dissertation.
+
+    Parameters:
+        hs (merger_system.HostSystem)   : The object describing the properties of the host system
+        ev (inspiral.Classic.Evolution) : The evolution object that results from the inspiral modeling
+        t_grid (array_like) : The times at which to evaluate the angles, will call the _int interpolation objects in ev
+
+    Returns:
+        iota_prime : np.ndarray
+            The inclination angle of the observer plane wrt the orbital plane
+        beta_prime : np.ndarray
+            The angle between the polarization axis in the observer frame to the periapse of the orbit
+    """
+    omega = ev.periapse_angle_int(t_grid)
+    Omega = ev.longitude_an_int(t_grid)
+    iota = ev.inclination_angle_int(t_grid)
+    iota_tilde = hs.inclination_angle
+
+    iota_prime = np.arccos(np.cos(iota)*np.cos(iota_tilde)
+                                    - np.sin(iota)*np.sin(iota_tilde)*np.cos(Omega)
+                                 )
+
+    if np.all(iota == 0.) or iota_tilde == 0.:
+        beta = (omega + Omega)
+
+    elif np.all(Omega == 0.):
+        beta = omega
+
+    elif np.all(omega == 0.):
+        omega_tilde = np.arctan2(np.sin(Omega)*np.sin(iota_tilde),
+                                 np.cos(Omega) *np.cos(iota)*np.sin(iota_tilde)
+                                  + np.sin(iota)*np.cos(iota_tilde))
+
+        Omega_tilde = np.arctan2( np.sin(Omega)*np.sin(iota),
+                                   np.sin(iota)*np.cos(Omega)*np.cos(iota_tilde)
+                                     + np.sin(iota_tilde)*np.cos(iota))
+        beta = (omega_tilde + Omega_tilde)
+
+    else:
+        omega_tilde = -np.arctan2( ( np.cos(Omega) * np.sin(omega) * np.cos(iota)
+                                        + np.sin(Omega) * np.cos(omega) ) * np.sin(iota_tilde)
+                                     + np.sin(iota) * np.sin(omega) * np.cos(iota_tilde),
+
+                                     ( -np.cos(Omega) * np.cos(omega) * np.cos(iota)
+                                       + np.sin(Omega) * np.sin(omega) ) * np.sin(iota_tilde)
+                                     - np.sin(iota) * np.cos(omega) * np.cos(iota_tilde)
+                               )
+        Omega_tilde = np.arctan2( np.sin(Omega)*np.sin(iota),
+                                   np.sin(iota)*np.cos(Omega)*np.cos(iota_tilde)
+                                     + np.sin(iota_tilde)*np.cos(iota))
+
+        beta = (omega_tilde + Omega_tilde)
+    return iota_prime, beta
+
+
 def h(hs, ev, t_grid, phi_0=0., acc=1e-13):
     """
     This function calculates the time domain gravitational waveform h_+,x(t) for eccentric inspirals according to eq (96) in https://arxiv.org/pdf/2107.00741.pdf
+    Should be identical to h_projected, up to numerical inaccuracies.
+
     Parameters:
         hs (merger_system.HostSystem)   : The object describing the properties of the host system
         ev (inspiral.Classic.Evolution) : The evolution object that results from the inspiral modeling
@@ -169,30 +234,84 @@ def h(hs, ev, t_grid, phi_0=0., acc=1e-13):
         h_cross : np.ndarray
             The amplitude of the cross polarization waveform at the corresponding time steps of t_grid
     """
-    a_int = interp1d(ev.t, ev.a, kind='cubic', bounds_error=False, fill_value=(0.,0.))
-    if  isinstance(ev.e, (collections.Sequence, np.ndarray)):
-        e_int = interp1d(ev.t, ev.e, kind='cubic', bounds_error=False, fill_value=(0.,0.))
-    else:
-        e_int = interp1d(ev.t, np.zeros(np.shape(ev.t)), bounds_error=False, fill_value=(0.,0.))
 
     def phi_dot(t, phi):  # The orbital phase evolution according to Maggiore (2007)
-        return np.sqrt(ev.m_tot/a_int(t)**3) * (1. - e_int(t)**2)**(-3./2.) * (1. + e_int(t)*np.cos(phi))**2
+        return np.sqrt(ev.m_tot/ev.a_int(t)**3) * (1. - ev.e_int(t)**2)**(-3./2.) * (1. + ev.e_int(t)*np.cos(phi))**2
 
     sol = solve_ivp(phi_dot, [t_grid[0], t_grid[-1]], [phi_0], t_eval=t_grid, rtol=acc, atol=acc)  # calculate the orbital phase at the given time steps t_grid
     phi = sol.y[0]
-    e = e_int(t_grid)
+    e = ev.e_int(t_grid)
 
-    h_plus = - ( (2. * np.cos(2.*phi - 2.*ev.periapse_angle) + 5.*e/2. * np.cos(phi - 2.*ev.periapse_angle)
-                    + e/2. * np.cos(3.*phi - 2.*ev.periapse_angle) + e**2 * np.cos(2.*ev.periapse_angle) ) * (1. + np.cos(ev.inclination_angle)**2 )
-                + (e * np.cos(phi) + e**2 ) * np.sin(ev.inclination_angle)**2 )
-    h_plus *= ev.m_red*ev.m_tot / (a_int(t_grid)*(1. - e**2)) / hs.D_l
+    inclination_angle, beta = get_observer_orbit_angles(hs, ev, t_grid)
 
-    h_cross = - (4. * np.sin(2.*phi - 2.*ev.periapse_angle) + 5.*e * np.sin(phi - 2.*ev.periapse_angle)
-                        + e * np.sin(3.*phi - 2.*ev.periapse_angle) - 2.*e**2 * np.sin(2.*ev.periapse_angle)) * np.cos(ev.inclination_angle)
-    h_cross *= ev.m_red*ev.m_tot / (a_int(t_grid)*(1. - e**2)) / hs.D_l
+    h_plus = - ( (2. * np.cos(2.*phi + 2.*beta) + 5.*e/2. * np.cos(phi + 2.*beta)
+                    + e/2. * np.cos(3.*phi + 2.*beta) + e**2 * np.cos(2.*beta) ) * (1. + np.cos(inclination_angle)**2 )
+                + (e * np.cos(phi) + e**2 ) * np.sin(inclination_angle)**2 )
+    h_plus *= ev.m_red*ev.m_tot / (ev.a_int(t_grid)*(1. - e**2)) / hs.D_l
+
+    h_cross = - (4. * np.sin(2.*phi + 2.*beta) + 5.*e * np.sin(phi + 2.*beta)
+                        + e * np.sin(3.*phi + 2.*beta) + 2.*e**2 * np.sin(2.*beta)) * np.cos(inclination_angle)
+    h_cross *= ev.m_red*ev.m_tot / (ev.a_int(t_grid)*(1. - e**2)) / hs.D_l
 
     return h_plus, h_cross
 
+def h_projected(hs, ev, t_grid, phi_0=0., acc=1e-13):
+    """
+    This function calculates the time domain gravitational waveform h_+,x(t) for eccentric inspirals
+        according to 11.2.2 in Poisson, Will (2014)
+        by calculating the emissions in the fundamental frame and projecting it onto the observer plane.
+    Should be identical with h(), up to numerical inaccuracies
+
+    Parameters:
+        hs (merger_system.HostSystem)   : The object describing the properties of the host system
+        ev (inspiral.Classic.Evolution) : The evolution object that results from the inspiral modeling
+        t_grid (array_like) : The times at which to evaluate h
+        phi_0 (float)   : The initial phase of the orbit at t_grid[0]
+        acc    (float)  : An accuracy parameter that is passed to the integration function
+
+    Returns:
+        h_plus : np.ndarray
+            The amplitude of the plus polarization waveform at the corresponding time steps of t_grid
+        h_cross : np.ndarray
+            The amplitude of the cross polarization waveform at the corresponding time steps of t_grid
+
+    Comments:
+        We have three frames here: The orbital plane of the inspiral, the fundamental frame of the hs system,
+        and the observers frame. The angles in the ev object give the orbital plane wrt to the fundamental frame,
+        and the angles in the hs system give the fundamental frame wrt to the observers frame.
+        The waveforms can be described in the orbital plane and then rotated into the two other frames consecutively
+    """
+    # First, we have to get the phase of the secondary on t_grid
+    def phi_dot(t, phi):  # The orbital phase evolution according to Maggiore (2007)
+        return np.sqrt(ev.m_tot/ev.a_int(t)**3) * (1. - ev.e_int(t)**2)**(-3./2.) * (1. + ev.e_int(t)*np.cos(phi))**2
+
+    sol = solve_ivp(phi_dot, [t_grid[0], t_grid[-1]], [phi_0], t_eval=t_grid, rtol=acc, atol=acc)  # calculate the orbital phase at the given time steps t_grid
+    phi_grid = sol.y[0]
+
+    # Go from the fundamental frame to the observers frame to obtain polarization axis
+    ko_observer = KeplerOrbit(None, None, None, inclination_angle=hs.inclination_angle)
+    X_obs = ko_observer.from_fundamental_xy_plane_to_orbital_xy_plane(np.array([1.,0.,0.]))
+    Y_obs = ko_observer.from_fundamental_xy_plane_to_orbital_xy_plane(np.array([0.,1.,0.]))
+
+    # Calculate the waveforms
+    h_plus, h_cross = np.zeros(np.shape(t_grid)), np.zeros(np.shape(t_grid))
+    for i, (t, phi) in enumerate(zip(t_grid, phi_grid)):
+        ko = ev.get_kepler_orbit(t, interpolate=True)
+        n, m, _ = ko.get_orbital_vectors_in_fundamental_xy_plane(phi)
+
+        h_jk = (- (1. + ko.e* np.cos(phi) - ko.e**2 * np.sin(phi)**2) * np.outer(n,n)
+                + ko.e*np.sin(phi) * (1.+ ko.e*np.cos(phi))*(np.outer(n,m) + np.outer(m,n))
+                + (1.+ko.e*np.cos(phi))**2 * np.outer(m, m) )
+
+        # Project to get polarizations
+        h_plus[i] = np.matmul(X_obs.T, np.matmul(h_jk, X_obs)) -  np.matmul(Y_obs.T, np.matmul(h_jk, Y_obs))
+        h_cross[i] = np.matmul(X_obs.T, np.matmul(h_jk, Y_obs)) +  np.matmul(Y_obs.T, np.matmul(h_jk, X_obs))
+
+    # Multiply to get the right units
+    h_plus *= 2.*ev.m_red*ev.m_tot / (ev.a_int(t_grid)*(1. - ev.e_int(t_grid)**2)) / hs.D_l
+    h_cross *= 2.*ev.m_red*ev.m_tot / (ev.a_int(t_grid)*(1. - ev.e_int(t_grid)**2)) / hs.D_l
+
+    return h_plus, h_cross
 
 def N_cycles_n(n, hs, ev, acc=1e-13):
     """
